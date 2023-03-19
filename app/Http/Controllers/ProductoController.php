158f24684ef9418d997eb2362e2ae06d9b33cb23
Sebastian Sepulveda <isurus.1313@gmail.com>
2023-03-18 21:24:56 -0300

    rutasyController

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ProductoController extends Controller {
+
+    public function index(){
+        return "Controller catalogo de productos";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('productos', [ProductoController::class, 'index']);
+
+
+Route::get('productos/{producto}', function ($producto) {
+    return "Detalle del Producto $producto";
+});
+
+Route::get('clientes/{id?}', function ($id = 1) { //valor predeterminado
+    return "Cliente $id";
+});
+
+Route::get('clientes/{id}/venta/{idVenta?}', function ($id = 1, $idVenta = null) { //valor predeterminado
+    if ($idVenta == null) {
+        return "ERROR";
+    }
+    return "El cliente $id realizo la venta $idVenta";
+});
+
+Route::get('/pruebas/(nombre?)', function ($nombre = null) {
+    $texto = "<h1>Hola Mundo desde ruta</h1>"; //Este texto viene desde una view
+    $texto .= 'Nombre: ' . $nombre;
+
+    return view('pruebas', array(
+        'texto' => $texto
+    ));
+  });
+  
+
 Route::get('/', function () {
-    return view('welcome');
+  //  return view('welcome');
+  return "<h1>Hola Mundo</h1>";
 });
